9dfbf418001caf951767dcc52ce8698980cb711c
Mohammad Saleh <scrolsin333@gmail.com>
2023-07-10 21:30:09 +0330

    first commit
    new files:
    
    hello_php
    variables

---
+++ variables.php
@@ -0,0 +1,26 @@
+<?php
+
+$int = 12;
+$float = 41.3;
+
+$string = "this is a string";
+
+$bool = true;
+$bool = false;
+
+$array = array("string", 12, 41.3, true);
+
+
+var_dump($int);
+echo "<br>";
+
+var_dump($float);
+echo "<br>";
+
+var_dump($string);
+echo "<br>";
+
+var_dump($bool);
+echo "<br>";
+
+var_dump($array);
