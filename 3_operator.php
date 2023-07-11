73615155f41aee09ed067075436a1f0268e4148d
Mohammad Saleh <scrolsin333@gmail.com>
2023-07-12 00:05:17 +0330

    update operator

---
+++ 3_operator.php
@@ -0,0 +1,85 @@
+<?php
+//
+//
+//$a = 8;
+//$b = 2;
+//$c = 12.8;
+//
+//$d = 0;
+//
+//$c = "12.5";
+//
+//var_dump($a - $b);echo "<hr>";
+//var_dump($a + $b);echo "<hr>";
+//
+//var_dump($a * $b);echo "<hr>";
+//var_dump($a / $b);echo "<hr>";
+//
+//var_dump($a % $b);echo "<hr>";
+//var_dump($b ** $a);echo "<hr>";
+//
+//var_dump($c);echo "<hr>";
+//var_dump((int) $c);echo "<hr>";
+//
+//var_dump($d);echo "<hr>";
+//var_dump((bool) $d);echo "<hr>";
+//
+//var_dump($c);echo "<hr>";
+//var_dump((int) $c);echo "<hr>";
+//
+//$b = $b + 1;
+//
+//$var = $b++;
+//
+//echo "<hr>";
+//
+//echo $var;echo "<hr>";
+//echo $b;echo "<hr>";
+//
+//$var = ++$b;
+//
+//echo "<hr>";
+//
+//echo $var;echo "<hr>";
+//echo $b;
+//
+//
+//echo "<hr>";
+//
+//var_dump( true == false);echo "<hr>";
+//var_dump("1" == 1);echo "<hr>";
+//var_dump(1 === "1");echo "<hr>";
+//
+//
+//var_dump(true and true);echo "<hr>";
+//var_dump(true or false);echo "<hr>";
+//
+//var_dump(false xor false);echo "<hr>";
+//var_dump(! (false xor false));echo "<hr>";
+//
+//$var =+ 5;
+//echo $var;
+//
+//echo "<hr>";
+//
+//$var += 5;
+//echo $var;
+//
+//echo "<hr>";
+//
+//
+$number_1 = 13;
+$number_2 = 34;
+$number_3 = 13;
+//
+//var_dump($number_1 < $number_2);echo "<hr>";
+//
+//var_dump($number_2 < $number_1);echo "<hr>";
+//
+//var_dump($number_1 < $number_3);echo "<hr>";
+//
+//var_dump($number_3 <= $number_1);echo "<hr>";
+
+var_dump($number_2 <=> $number_1);echo "<hr>";
+var_dump($number_1 <=> $number_2);echo "<hr>";
+var_dump($number_1 <=> $number_3);echo "<hr>";
