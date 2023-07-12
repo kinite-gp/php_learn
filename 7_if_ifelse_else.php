f2529d50dc00b78364b3cdd32889ffee451c4197
Mohammad Saleh <scrolsin333@gmail.com>
2023-07-13 00:01:53 +0330

    add if and switch
    new files:
    
    if_ifelse_else
    switch_case

---
+++ 7_if_ifelse_else.php
@@ -0,0 +1,19 @@
+<?php
+
+
+$value = 34;
+
+
+if ($value == 1) {
+    echo "value is $value";
+} elseif ($value == 2) {
+    echo "value is $value";
+} elseif ($value == 3) {
+    echo "value is $value";
+} elseif ($value == 4) {
+    echo "value is $value";
+} else {
+    echo "value is different!";
+}
+
+
---
+++ 8_switch_case.php
@@ -0,0 +1,16 @@
+<?php
+
+
+$value = 3;
+
+
+switch ($value) {
+    case 1:
+    case 2:
+    case 3:
+    case 4:
+        echo "value is $value";
+        break;
+    default:
+        echo "value is different!!";
+}
